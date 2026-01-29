d17f02fdc23a4385b799930aea200869a27bc654
guilherme-teles-dev <guilherme.teles@ufms.br>
2026-01-29 17:56:59 -0400

    add state machine basic code

---
+++ estado_turno_jogador.gd
@@ -0,0 +1,10 @@
+extends EstadoJogo
+
+func entrar():
+	print("--- [ESTADO] Turno do Jogador ---")
+	print("Esperando o jogador jogar uma carta... (Input liberado)")
+
+func sair():
+	print("Saindo do turno do jogador...")
+
+# Aqui usaremos o _input mais tarde para detectar o clique na carta
